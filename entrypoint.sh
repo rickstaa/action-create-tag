87a112438ed8b80a2ae25dcf8f11d4949dc4f740
Varya Stepanova <mail@varya.me>
2022-11-18 14:04:45 +0200

    feat: no_verify_tag option to skip verification when pushing the tag (#24)
    * no_verify_tag option to skip verification when pushing the tag
    
    * refactor: clean up code
    
    * fix: fix action output message bug
    
    * refactor: clean up code
    
    * fix: fix sh syntax error
    
    * refactor: update code style
    
    Co-authored-by: rickstaa <rick.staa@outlook.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-# Apply hotfix for 'fatal: unsafe repository' error (see #10)
+# Apply hotfix for 'fatal: unsafe repository' error (see #10).
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 
 cd "${GITHUB_WORKSPACE}" || exit
@@ -12,18 +12,23 @@ if [ -z "${INPUT_TAG}" ]; then
 fi
 
 # Set up variables.
+FLAGS=""
 TAG=$(echo "${INPUT_TAG}" | sed 's/ /_/g')
+ACTION_OUTPUT_MESSAGE="[action-create-tag] Push tag '${TAG}'"
 MESSAGE="${INPUT_MESSAGE:-Release ${TAG}}"
 FORCE_TAG="${INPUT_FORCE_PUSH_TAG:-false}"
+NO_VERIFY="${INPUT_NO_VERIFY_TAG:-false}"
 SHA=${INPUT_COMMIT_SHA:-${GITHUB_SHA}}
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-# Create tag
+# Create tag and handle force push action input.
 echo "[action-create-tag] Create tag '${TAG}'."
 if [ "${FORCE_TAG}" = 'true' ]; then
   git tag -fa "${TAG}" "${SHA}" -m "${MESSAGE}"
+  FLAGS="${FLAGS} --force"
+  ACTION_OUTPUT_MESSAGE="${ACTION_OUTPUT_MESSAGE}, with --force"
 else
   git tag -a "${TAG}" "${SHA}" -m "${MESSAGE}"
 fi
@@ -33,11 +38,12 @@ if [ -n "${INPUT_GITHUB_TOKEN}" ]; then
   git remote set-url origin "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 fi
 
-# Push tag
-if [ "${FORCE_TAG}" = 'true' ]; then
-  echo "[action-create-tag] Force push tag '${TAG}'."
-  git push --force origin "${TAG}"
-else
-  echo "[action-create-tag] Push tag '${TAG}'."
-  git push origin "${TAG}"
+# Handle no-verify action input.
+if [ "${NO_VERIFY}" = 'true' ]; then
+  FLAGS="${FLAGS} --no-verify"
+  ACTION_OUTPUT_MESSAGE="${ACTION_OUTPUT_MESSAGE}, with --no-verify"
 fi
+
+# Push tag.
+echo "${ACTION_OUTPUT_MESSAGE}"
+git push $FLAGS origin "$TAG"
